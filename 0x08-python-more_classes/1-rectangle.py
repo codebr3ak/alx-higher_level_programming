d90f4868464167b1c6092ef81b76056eb81e4d64
codebr3ak <barakaralph@gmail.com>
2023-01-11 17:01:04 +0300

    corrections

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Defines a Rectangle class."""
 
 
@@ -40,4 +39,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value  
-         
