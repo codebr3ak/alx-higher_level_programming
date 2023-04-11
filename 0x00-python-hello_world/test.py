a95a78879f6e28d571624d5fabc6c91bd332ecae
codebr3ak <barakaralph@gmail.com>
2023-04-11 14:22:26 +0300

    Back FCrom The VDacay

--- 0x00-python-hello_world/test.py
+++ 0x00-python-hello_world/test.py
@@ -1,3 +1,18 @@
-for i in [1, 3, 4, 2]:
-    print(i, end=" ")
-    
+class Base():
+    """ My base class """
+
+    __nb_instances = 0
+
+    def __init__(self):
+        Base.__nb_instances += 1
+        self.id = Base.__nb_instances
+
+class User(Base):
+    """ My User class """
+
+    def __init__(self):
+        super().__init__()
+        self.id += 99
+
+u = User()
+print(u.id)
